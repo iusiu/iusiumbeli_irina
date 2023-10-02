2b6a46c8781c203f83b28ba5f708c6ca6752eae7
Iusiumbeli Irina <iusiumbeli83015@mail.ru>
2023-10-02 22:37:35 +0300

    Done Homework Nr 9

---
+++ src/main/java/homework_nr_9/Main.java
@@ -0,0 +1,14 @@
+package homework_nr_9;
+
+public class Main {
+    public static void main(String[] args) {
+        Animal e = new Elephant();
+        e.eat();
+        e.makeSound();
+        e.doSomething();
+        Herbivore elephant = new Elephant();
+        elephant.afraidOf();
+        elephant.hide();
+        }
+
+    }
